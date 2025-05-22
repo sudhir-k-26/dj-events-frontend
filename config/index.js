c4f41eb8fd3bbcc8d9c0a273dd5420c4cbb5a52e
sudhir-k-26 <56623985+sudhir-k-26@users.noreply.github.com>
2025-05-22 20:18:03 +0530

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:1337';
+  process.env.NEXT_PUBLIC_API_URL || 'https://dj-events-backend-ia84.onrender.com';
 export const PER_PAGE = 2;
 export const NEXT_URL =
-  process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://localhost:3000';
+  process.env.NEXT_PUBLIC_FRONTEND_URL || 'https://djeventsfrontend2.vercel.app';
